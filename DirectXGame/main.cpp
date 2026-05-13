518521d07b67d1f780075b5769f650feca1bb118
yaritakoya <k024g0053@g.neec.ac.jp>
2026-05-13 13:08:23 +0900

    プロジェクトファイルを追加。

---
+++ DirectXGame/main.cpp
@@ -0,0 +1,33 @@
+#include "KamataEngine.h"
+#include <Windows.h>
+
+using namespace KamataEngine;
+
+// Windowsアプリでのエントリーポイント(main関数)
+int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
+
+	// エンジンの初期化
+	KamataEngine::Initialize(L"LE3D_25_ヤリタ_コウヤ_CG5");
+
+	// DirectXCommonインスタンスの取得
+	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
+
+	// メインループ
+	while (true) {
+		// エンジンの更新
+		if (KamataEngine::Update()) {
+			break;
+		}
+
+		// 描画開始
+		dxCommon->PreDraw();
+
+		// 描画終了
+		dxCommon->PostDraw();
+	}
+
+	// エンジンの終了処理
+	KamataEngine::Finalize();
+
+	return 0;
+}
